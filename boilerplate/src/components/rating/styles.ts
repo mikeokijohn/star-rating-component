b45fcea3a1edfc88030d77c87c0a3a9c3ba19187
Michael John <53580582+mikeokijohn@users.noreply.github.com>
2022-06-15 23:08:28 +0200

    Styling for label and form fieldset reset

--- boilerplate/src/components/rating/styles.ts
+++ boilerplate/src/components/rating/styles.ts
@@ -2,7 +2,15 @@ import { CSSResultGroup } from "lit";
 import { css } from 'lit';
 
 export const ratingStyles: CSSResultGroup = css`
-    fieldset {
-        background-color: red;
+    .star-rating__fieldset {
+        display: block;
+        border: none;
+        padding: 0;
+    }
+    .star-rating__label {
+        position: relative;
+        display: inline-block;
+        height: 30px;
+        width: 30px;
     }
 `;
