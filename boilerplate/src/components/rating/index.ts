bef5c9bcab3bae5ec3f537b45dbb20d97490df3b
Michael John <53580582+mikeokijohn@users.noreply.github.com>
2022-06-15 22:36:55 +0200

    Use styles in component - only testing for now

--- boilerplate/src/components/rating/index.ts
+++ boilerplate/src/components/rating/index.ts
@@ -1,8 +1,12 @@
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { ratingStyles } from "./styles";
 
 @customElement(`case-rating`)
 export class CaseRating extends LitElement {
+  static styles = [
+    ratingStyles
+  ];
   render() {
     return html`<div>CaseRating component</div>`;
   }
